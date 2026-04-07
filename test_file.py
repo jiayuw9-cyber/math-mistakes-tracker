c7d423a084ca493d5e65322d7ae1c797f5ac4126
jiayuw9-cyber <jiayuw9@illinois.edu>
2026-04-07 10:25:13 -0500

    renamed test_file

--- test_file.py
+++ test_file.py
@@ -2,22 +2,22 @@ import unittest
 import os
 from mistake_manager import add_mistake, load_data, view_mistakes
 
-Test_file = "mistakes.txt"
+test_file = "mistakes.txt"
 
 class TestMathMistakeTracker(unittest.TestCase):
-    def setUp(self):
-        with open(Test_file,"w") as file:
+    def setup(self):
+        with open(test_file,"w") as file:
             file.write("")
     
     def test_add_mistakes(self):
-        add_mistake("1+1","3","2", Test_file)
-        data = load_data(Test_file)
+        add_mistake("1+1","3","2", test_file)
+        data = load_data(test_file)
 
         self.assertEqual(len(data), 1)
 
     def test_view_mistakes(self):
-        add_mistake("1+1","3","2", Test_file)
-        data = view_mistakes(Test_file)
+        add_mistake("1+1","3","2", test_file)
+        data = view_mistakes(test_file)
 
         self.assertEqual(len(data), 1)
 

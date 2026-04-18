20fa313c5ba2a16ba560e48162dfe48f728b5c0d
jiayuw9-cyber <jiayuw9@illinois.edu>
2026-04-17 23:08:49 -0500

    begin to code quiz

--- quiz.py
+++ quiz.py
@@ -1,1 +1,21 @@
+import random
+from main import load_mistakes 
 
+def quiz_user(filenmae):
+    mistakes = load_mistakes(filename)
+
+    if len(mistakes) == 0:
+        print("There are no mistakes to quiz.")
+        return
+    
+# Random choose 10 mistakes
+# split it into three parts
+#just show the question
+#make user print the answer 
+# (I am not sure whether I can choose one word as a key word in the answer, and if it is match it is correct.)
+#But if it is just numbers it should be match.
+# Print the output.
+
+
+#Also, if I have enough time, I can store the result of every times.(Or ten times?)
+#Make a conclusion to as bar or scatter plot.

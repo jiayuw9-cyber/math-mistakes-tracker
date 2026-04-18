6ac56a76a348e6ea42f1ef04e6cf74eabdbdf913
jiayuw9-cyber <jiayuw9@illinois.edu>
2026-04-17 22:24:50 -0500

    finish add_mistakes code.

--- main.py
+++ main.py
@@ -2,16 +2,19 @@
 #1. add_mistake
 #Allows the user to input a math problem and save the wrong answer and correct answer.
 #This function saves math mistakes into a text file.
+def add_mistake(filename):
+    question = input("Enter the math problem you got wrong (e.g., 5 + 3): ").strip()
+    wrong_answer = input("Enter the math question:").strip()
+    correct_answer = input("enter the correct answer:").strip()
 
-def add_mistake():
-question = input("Enter the math problem you got wrong (e.g., 5 + 3): ").strip()
-wrong_answer = input("Enter the math question:").strip()
-correct_answer = input("enter the correct answer:").strip()
+    if question == "" or wrong_answer == "" or correct_answer == "":
+        print("Error: All fields are required. Please try again.")
+        return
 
-if question == "" or wrong_answer == "" or correct_answer == "":
-    print("Error: All fields are required. Please try again.")
-    return
-
+    with open (filename,"a") as file:
+        file.write(question + " | " + wrong_answer + " | " + correct_answer + "\n")
+    
+    print("Mistakes saved successfully.")
 
 
 #2. view_mistakes

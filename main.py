d0622e7701ed7cd0f06c6929410d0dc3ef96b807
jiayuw9-cyber <jiayuw9@illinois.edu>
2026-04-17 22:16:02 -0500

    begin to code add_mistake

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import numpy as np
-from 
 
 #1. add_mistake
 #Allows the user to input a math problem and save the wrong answer and correct answer.
 #This function saves math mistakes into a text file.
 
-class Agent():
+def add_mistake():
+question = input("Enter the math problem you got wrong (e.g., 5 + 3): ").strip()
+wrong_answer = input("Enter the math question:").strip()
+correct_answer = input("enter the correct answer:").strip()
 
-
+if question == "" or wrong_answer == "" or correct_answer == "":
+    print("Error: All fields are required. Please try again.")
+    return
 
 
 

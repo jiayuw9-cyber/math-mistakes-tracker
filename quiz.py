ef4b8cf17ebf4d0ef1e98469f8544544c42384cd
jiayuw9-cyber <jiayuw9@illinois.edu>
2026-05-05 10:15:40 -0500

    finish pt1 in quiz to make the quiz function works.

--- quiz.py
+++ quiz.py
@@ -1,5 +1,34 @@
 import random
 from mistake_manager import load_mistakes 
+import matplotlib.pyplot as plt
+
+result_file = "quiz_result"
+# split it into three parts
+def save_mistakes(filename, mistakes):
+    with open(filename, "w") as file:
+        for question, wrong_answer, correct_answer in mistakes:
+            file.write(question + " | " + wrong_answer + " | " + correct_answer + "\n")
+
+def save_quiz_result(accuracy):
+    with open(result_file,"a") as file:
+            file.write(str(accuracy) + "\n")
+# ensure save in result_file
+
+def load_quiz_results():
+    results = []
+
+    try:
+         with open(result_file, "r") as file:
+              for line in file:
+                   line = line.strip()
+                   if line != "":
+                        results.append(float(line))
+
+    except FileNotFoundError:
+         return[]
+    
+    return results
+
 
 def quiz_user(filename):
     mistakes = load_mistakes(filename)
@@ -11,13 +40,41 @@ def quiz_user(filename):
 # Random choose 10 mistakes
     num_questions = min(10, len(mistakes))
     selected = random.sample(mistakes, num_questions)
-# split it into three parts
+
+    score = 0
+    still_wrong = []
 #just show the question
 #make user print the answer 
-# (I am not sure whether I can choose one word as a key word in the answer, and if it is match it is correct.)
-#But if it is just numbers it should be match.
-# Print the output.
+    for i, mistake in range(len(mistakes)):   #????
+        question, wrong_answer, correct_answer = mistake
+
+        print(f"\nQuestion{i}:{question}")
+        user_answer = input("Your answer: ").strip()
+
+        if user_answer == correct_answer.strip():
+            print("Correct!")
+            score += 1
+        else:
+            print(f"Wrong. Correct answer: {correct_answer}")
+            still_wrong.append(mistake)
+
+#keep mistakes which were not seected and the selected questions that are still wrong.
+    not_selected = []
+
+    for mistake in mistakes:
+        if mistake not in selected:
+             not_selected.append(mistake)
+
+    updated_mistakes = not_selected + still_wrong
+    save_mistakes(filename, updated_mistakes)
+
+    accuracy = score / num_questions
+    save_quiz_result(accuracy)
+
+    print("\nQuiz finished!")
+    print(f"Score: {score}/{num_questions}")
+    print(f"Accuracy: {accuracy * 100:.2f}%")
+    print("The question you answered correctly has already remved from the mistakes list!")
 
 
-#Also, if I have enough time, I can store the result of every times.(Or ten times?)
-#Make a conclusion to as bar or scatter plot.
+#show the conclusion as a line plot.

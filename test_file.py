a3e2cad8f132a236da8fa034644980a806e2ffc5
jiayuw9-cyber <jiayuw9@illinois.edu>
2026-05-09 15:11:47 -0500

    change load_data to load_mistakes, as it is not exist and my dict is error due to my data is tuple.

--- test_file.py
+++ test_file.py
@@ -1,28 +1,28 @@
 import unittest
 import os
-from mistake_manager import add_mistake, load_data, view_mistakes
+from mistake_manager import add_mistake, load_mistakes, view_mistakes
 
 test_file = "mistakes.txt"
 
 class TestMathMistakeTracker(unittest.TestCase):
-    def setup(self):
+    def setUp(self):    # I did setup instead of setUp, but it is not working, so I do the dubugging here using chatgpt.
         with open(test_file,"w") as file:
             file.write("")
     
     def test_add_mistakes(self):
         add_mistake("1+1","3","2", test_file)
-        data = load_data(test_file)
+        data = load_mistakes(test_file)
 
         self.assertEqual(len(data), 1)
 
     #def test_load_mistakes(self):
     def test_load_mistakes(self):
         add_mistake("1+1","3","2", test_file)
-        data = load_data(test_file)
+        data = load_mistakes(test_file)
 
-        self.assertEqual(data[0]["problem"], "1+1")
-        self.assertEqual(data[0]["wrong"], "3")
-        self.assertEqual(data[0]["correct"], "2")
+        self.assertEqual(data[0][0], "1+1")
+        self.assertEqual(data[0][1], "3")
+        self.assertEqual(data[0][2], "2")
 
 
     def test_view_mistakes(self):
